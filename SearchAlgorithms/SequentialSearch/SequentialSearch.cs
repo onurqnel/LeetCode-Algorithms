5a1822f3604878f65d7b0d0eb9eee6cdc43c9335
onurqnel <onur.onel2230@gmail.com>
2024-10-28 16:42:10 -0400

    Developed Sequential Search Algorithm

---
+++ SearchAlgorithms/SequentialSearch/SequentialSearch.cs
@@ -0,0 +1,23 @@
+﻿public class SequentialSearch
+{
+    public static void Main()
+    {
+        Console.Clear();
+
+        int[] numbers = new int[15] { 87, 75, 98, 100, 82, 14, 76, 3, 91, 34, 27, 4, 63, 12, 88 };
+        int target;
+
+        Console.Write("Enter an integer you wanted to search for: ");
+        target = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] == target)
+            {
+                Console.WriteLine("Target `{0}` foundend in position `{1}`.", numbers[i], i);
+                return;
+            }
+        }
+        Console.WriteLine("Target `{0}` was not found in the array.", target);
+    }
+}
